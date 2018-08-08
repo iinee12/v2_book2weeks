754cc291d91dae7f85f57007d7e51bc0e60761fe
JinHo <jjinho28@gmail.com>
2018-08-09 01:55:45 +0900

    logout delete

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
-from django.contrib.auth.views import logout
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
@@ -14,7 +13,6 @@ urlpatterns = [
     path('readingwrite/', views.readingWirte),
     path('readingdetail/', views.readingdetail),
     path('meetingmain/', views.meetingmain),
-    url(r'^accounts/logout/$', auth_views.logout, {'next_page' : '/login'}),
     url(r'^login/$', views.signin, name='login'),
     url(r'^summernote/', include('django_summernote.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
