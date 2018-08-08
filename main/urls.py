d446ddbb2b875da75ea41868de191c42d2aee64c
JinHo <jjinho28@gmail.com>
2018-08-09 01:46:46 +0900

    urls.py 수정

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import logout
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
