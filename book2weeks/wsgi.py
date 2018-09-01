60520c558273bb3bdfdaf86c0a92e2e373c17cf9
JinHo <jjinho28@gmail.com>
2018-09-01 11:19:33 +0900

    change wsgi.py setting

--- book2weeks/wsgi.py
+++ book2weeks/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "book2weeks.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "book2weeks.settings.production")
 
 application = get_wsgi_application()
