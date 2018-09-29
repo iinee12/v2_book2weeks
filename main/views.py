de298a46408edc801bce16354ae9597a0b8463db
iinee12 <jjinho28@gmail.com>
2018-09-29 23:34:26 +0900

    delete log

--- main/views.py
+++ main/views.py
@@ -110,7 +110,6 @@ def nextbook(request):
     makePkDateTime = "%04d%02d%02d%02d%02d%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
     if request.method == "POST":
         form = wishbookForm(request.POST)
-        print(form)
         if form.is_valid():
             post = form.save(commit=False)
             post.created = nowDateTime
